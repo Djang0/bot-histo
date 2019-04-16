635894ba1e14920c4a50dfff05534bd6e96f96cc
Ludovic Reenaers <ludovic.reenaers@gmail.com>
2019-04-16 11:59:44 +0200

    fix

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express')
 const path = require('path')
 var bodyParser = require('body-parser');
 var moment = require('moment');
-
+const PORT = process.env.PORT || 5000
 //https://lreenaers.ddns.net:8443/UZeLvHozfhwsL53eoAjetEYv
 var welcome = function(req, res) {
   res.json({});
