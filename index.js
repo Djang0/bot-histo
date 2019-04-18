d505e662297a493b3e83a607f3228b39a0498bee
Ludovic Reenaers <ludovic.reenaers@gmail.com>
2019-04-18 06:32:21 +0200

    1.0

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var request = require('request');
 const express = require('express')
-
+var moment = require('moment');
 const path = require('path')
 var bodyParser = require('body-parser');
 var moment = require('moment');
@@ -18,12 +18,19 @@ var app = express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/',  async (req, res) => {
-    request('https://lreenaers.ddns.net:8443/NoQTpyQstWqBIoUgRdBsgIWCu', function(error, response, body) {
-      const data=JSON.parse(body);
-      console.log(data);
-      const results = {data : data};
-      res.render('pages/db', results );
+  .get('/', async (req, res) => {
+    request('https://lreenaers.ddns.net:8443/UZeLvHozfhwsL53eoAjetEYv', function(error, response, body) {
+      var data = JSON.parse(body);
+
+      // for (var key in data) {
+      //   if (data.hasOwnProperty(key)) {
+      //     var val = data[key];
+      //     per_command.ls.push({category: key, value: val.length});
+      //   }
+      // }
+
+
+      res.render('pages/db', {data:data, moment : moment});
     });
 
   })
