dc6ffe8df3ef7ffbb1a63ef7bbff2d960eb923fa
Ludovic Reenaers <ludovic.reenaers@gmail.com>
2019-08-10 15:45:39 +0200

    req forward

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 var moment = require('moment');
 const path = require('path')
 var bodyParser = require('body-parser');
-var moment = require('moment');
+
 const PORT = process.env.PORT || 5000
 //https://lreenaers.ddns.net:8443/UZeLvHozfhwsL53eoAjetEYv
 var welcome = function(req, res) {
@@ -39,5 +39,17 @@ var app = express()
         res.render('pages/usr', {data:data, moment : moment, usr_id:param_usr_id});
       });
 
+  })
+  .get('/file/:filename/:ebfs/:type', async (req, res) => {
+    var filename = req.params.filename;//ie: OA.19.08.08.txt
+    var ebfs = req.params.ebfs;//ie 1008
+    var type = req.params.type;//Flight_Level | Meter_Level
+    var formData = {
+      download_direct_oa_level_type: type,
+      download_direct_oa_pressure: ebfs,
+      download_direct_oa_file_real_name: filename,
+
+    };
+    request.post('http://mids.be/notam/bpc/download_direct_oa.php', {form: formData}).pipe(res);
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
